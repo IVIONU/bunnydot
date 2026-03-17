f8a95487d829c29de5bd075b5d52a3b9deb38de7
IVIONU <a2398206@naver.com>
2026-03-17 23:02:18 +0900

    디테일 수정

--- src/App.js
+++ src/App.js
@@ -7,11 +7,41 @@ const GlobalStyle = createGlobalStyle`
     margin:0;
     padding: 0;
     box-sizing:border-box;
+    font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui,
   }
   body {
     background-color: navy;
     color: white;
+    min-height: 100vh;
   }
+
+#root, .App {
+  background: transparent !important;
+}
+
+h1 {
+  font-size: 1.5rem;
+  margin: 30px 0;
+  font-weight: 600;
+  text-align: center;
+}
+
+.name {
+  font-size: 0.9rem;
+  margin: 15px 0 30px 0;
+  text-align: center;
+  opacity: 0.9;
+}
+
+img {
+  max-width: 280px;
+  width: 70%
+  height: auto;
+  border-radius: 15px;
+  margin: 0 auto;
+  display: block;
+  box-shadow: 0 4px 15px rgba(0,0,0,0.3);
+}
 `;
 
 function App() {
