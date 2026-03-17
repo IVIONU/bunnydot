06c3770de3790c8c2ed3acb523b33fc14666571a
IVIONU <a2398206@naver.com>
2026-03-17 22:17:13 +0900

    코드 변경

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,20 +3,92 @@ import { FlexBox } from "./style";
 
 const items = [
   {
-    name: "햄버거",
-    src: require("../../img/burger.jpg")
+    name: "251230",
+    src: require("../../img/bunnydot1.jpg")
   },
   {
-    name: "볶음밥",
-    src: require("../../img/fried-rice.jpg")
+    name: "260110",
+    src: require("../../img/bunnydot2.jpg")
   },
   {
-    name: "파스타",
-    src: require("../../img/pasta.jpg")
+    name: "260116",
+    src: require("../../img/bunnydot3.jpg")
   },
   {
-    name: "라면",
-    src: require("../../img/ramen.jpg")
+    name: "260113",
+    src: require("../../img/bunnydot4.jpg")
+  },
+    {
+    name: "260122",
+    src: require("../../img/bunnydot5.jpg")
+  },
+    {
+    name: "260130",
+    src: require("../../img/bunnydot6.jpg")
+  },
+    {
+    name: "260127",
+    src: require("../../img/bunnydot7.jpg")
+  },
+    {
+    name: "260125",
+    src: require("../../img/bunnydot8.jpg")
+  },
+    {
+    name: "260206",
+    src: require("../../img/bunnydot9.jpg")
+  },
+    {
+    name: "260207",
+    src: require("../../img/bunnydot10.jpg")
+  },
+    {
+    name: "260216",
+    src: require("../../img/bunnydot11.jpg")
+  },
+    {
+    name: "260211",
+    src: require("../../img/bunnydot12.jpg")
+  },
+    {
+    name: "260211",
+    src: require("../../img/bunnydot13.jpg")
+  },
+    {
+    name: "260210",
+    src: require("../../img/bunnydot14.jpg")
+  },
+    {
+    name: "260301",
+    src: require("../../img/bunnydot15.jpg")
+  },
+    {
+    name: "260227",
+    src: require("../../img/bunnydot16.jpg")
+  },
+    {
+    name: "260225",
+    src: require("../../img/bunnydot17.jpg")
+  },
+      {
+    name: "260303",
+    src: require("../../img/bunnydot18.jpg")
+  },
+      {
+    name: "260304",
+    src: require("../../img/bunnydot19.jpg")
+  },
+      {
+    name: "260307",
+    src: require("../../img/bunnydot20.jpg")
+  },
+      {
+    name: "260308",
+    src: require("../../img/bunnydot21.jpg")
+  },
+      {
+    name: "260306",
+    src: require("../../img/bunnydot22.jpg")
   }
 ];
 
@@ -48,7 +120,7 @@ const Game = () => {
   };
   return (
     <FlexBox>
-      <h1 className="title">Favorite Worldcup</h1>
+      <h1 className="title">25-26 ETERNITY 버니닷꾸</h1>
       {displays.map(d => {
         return (
           <div className="flex-1" key={d.name} onClick={clickHandler(d)}>
